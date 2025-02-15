47015c62797d163c9214f1da1867be4401f94a17
ExposedGuard <exposedguard@gmail.com>
2025-02-15 23:26:31 +0100

    Logs

--- config.lua
+++ config.lua
@@ -1,9 +1,9 @@
 Config = {
     ['ip'] = true, -- IP Address ned to show in logs (true: shows, false: hide)
     ['logs'] = {
-        ['connect'] = "https://discord.com/api/webhooks/1337202490759839784/fhEkHn8xNi7RreJi0ha4wTkxvNxYd5OQ-n3GjON1wvLNpCS0YisHGrChts6k6BUW8xea", -- Player Connect (Discord Webhook)
-        ['disconnect'] = "https://discord.com/api/webhooks/1337202581377781760/r4NuO_Fp46fCS7l0lK8prrrXEaiqR3ONg_CCHWPh3TgHfegZtIx2OlztrjVgb-2wksMG", -- Player Discocnnect (Discord Webhook)
-        ['blocked'] = "https://discord.com/api/webhooks/1337202526595977257/AvppP5w87_9n8My-beXikWnfOh3dM0KsKh0KLPZFG-P4s0iASnXUbDuwO7b554odHdMs", -- Blocked Connection (Discord Webhook)
+        ['connect'] = "", -- Player Connect (Discord Webhook)
+        ['disconnect'] = "", -- Player Discocnnect (Discord Webhook)
+        ['blocked'] = "", -- Blocked Connection (Discord Webhook)
     },
     ["whitelist"] = { -- All exposed users do not get kicked
         "", -- Discord ID
