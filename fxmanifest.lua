e31fb52f9e5ffd9440e3dc51cec815e1bf8f851b
Ghost_the_killer24 <88981553+24-ghost-the-killer@users.noreply.github.com>
2025-03-03 18:48:26 +0100

    Performance Update

--- fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-
-author 'ExposedGuard - Blacklist'
-description 'Lavet af ExposedGuard'
-version '1.0.5'
-
-server_scripts { 'source/*.lua' }
-shared_scripts { 'config.lua', 'locales.lua' }
